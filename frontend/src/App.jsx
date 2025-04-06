2830a89e890e963fbd5d284237685f9d1b1238dc
Salman <salmannajah123@gmail.com>
2025-04-06 17:00:54 +0530

    Enhanced Routing and Logout feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,27 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import GoogleLogin from './components/GoogleLogin'
 import Dashboard from './components/Dashboard'
 import LandingPage from './components/LandingPage'
 import ErrorPage from './components/ErrorPage'
+import { useState } from 'react'
+import RefreshHandler from './RefreshHandler'
 
 function App() {
-  
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  //To prevent non authenticated users from accessing the dashboard
+  const PrivateRoute = ({ element }) => {
+    return isAuthenticated ? element : <Navigate to="/" />
+  }
+
   return (
     <BrowserRouter>
+      <RefreshHandler setIsAuthenticated={setIsAuthenticated} />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<GoogleLogin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
